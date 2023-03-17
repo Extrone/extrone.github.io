3d5d8f92378530a6efeb8881371af2ad6e068869
Arjun <31234599+Extrone@users.noreply.github.com>
2023-03-17 14:26:15 +0530

    Contact form test

---
+++ content/action_page.php
@@ -0,0 +1,15 @@
+<?php
+$action = $_REQUEST['action'];
+
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email'];
+$message = $_REQUEST['message'];
+if (($name == "") || ($email == "") || ($message == "")) {
+    echo "All fields are required, please fill <a href=\"\">the form</a> again.";
+} else {
+    $from = "From: $name<$email>\r\nReturn-path: $email";
+    $subject = "Message sent using your contact form";
+    mail("anjana4760@gmail.com", $subject, $message, $from);
+    echo "Email sent!";
+}
+?>
